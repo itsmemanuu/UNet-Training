ba575b05dd6a036422efd5bc08aa19c0827b4d00
Manuela Pacheco Malagón <m.pachecom2@uniandes.edu.co>
2023-05-31 16:27:01 -0500

    model, dataset and train

---
+++ train.py
@@ -0,0 +1,24 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import UNET
+# from utils import(load_checkpoint, save_checkpoint, get_loaders, check_accuracy, save_predictions_as_imgs)
+
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 160
+IMAGE_WIDTH = 240
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "data/train_images/"
+TRAIN_MASK_DIR = "data/train_masks/"
+VAL_IMG_DIR = "data/val_images/"
+VAL_MASK_DIR = "data/val_masks/"
+
+
